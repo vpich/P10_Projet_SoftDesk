34be0222b5cc44259beb49014b9a713eb7dce7cd
Virginie PICH <red_iv@hotmail.fr>
2023-05-05 15:43:46 +0200

    Modification du modèle User pour qu'il utilise un CustomUserManager permettant de ne pas utiliser le champ username

--- softdesk/authentication/models.py
+++ softdesk/authentication/models.py
@@ -1,5 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, first_name=None, last_name=None, **extra_fields):
+        if not email:
+            raise ValueError("Enter an email address")
+        if not first_name:
+            raise ValueError("Enter a first name")
+        if not last_name:
+            raise ValueError("Enter a last name")
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, first_name, last_name):
+        user = self.create_user(email, password=password, first_name=first_name, last_name=last_name)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
@@ -11,3 +33,8 @@ class User(AbstractUser):
         "first_name",
         "last_name",
     ]
+
+    def __str__(self):
+        return self.email
+
+    objects = CustomUserManager()
