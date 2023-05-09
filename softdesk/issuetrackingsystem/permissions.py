dcdd9c1366bb066b2a937b31ca41898b73a45180
Virginie PICH <red_iv@hotmail.fr>
2023-05-09 17:05:48 +0200

    Suppression des méthodes de CustomViewset pour intégrer les limitations dans permissions.py + remplacement des méthodes perform_create pour create, permettant d'obtenir une réponse sans faire de raise

--- softdesk/issuetrackingsystem/permissions.py
+++ softdesk/issuetrackingsystem/permissions.py
@@ -1,4 +1,5 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from issuetrackingsystem.models import Project, Contributor, Issue, Comment
 
 
 class IsAdminAuthenticated(BasePermission):
@@ -9,3 +10,38 @@ class IsAdminAuthenticated(BasePermission):
             and request.user.is_authenticated
             and request.user.is_superuser
         )
+
+
+def get_project(obj):
+    if type(obj) == Issue:
+        obj = obj.project
+    elif type(obj) == Comment:
+        obj = obj.issue_id.project
+    elif type(obj) == Contributor:
+        obj = obj.project
+    return obj
+
+
+class IsUserContributor(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        contributor = Contributor.objects.filter(project=get_project(obj), user=request.user)
+        if contributor:
+            return True
+        else:
+            return False
+
+
+class IsOwnerOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        if type(obj) == Project:
+            contributor = Contributor.objects.get(project=obj, user=request.user)
+            return contributor.role == contributor.Role.CREATOR
+        elif type(obj) == Contributor:
+            return obj.role == Contributor.Role.CREATOR
+        else:
+            return obj.author_user_id == request.user
--- softdesk/issuetrackingsystem/views.py
+++ softdesk/issuetrackingsystem/views.py
@@ -1,13 +1,15 @@
+import http
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from django.db.models import Q
+from rest_framework import status
 
 from issuetrackingsystem.models import Project, Issue, Comment, Contributor
 from issuetrackingsystem.serializers import ProjectDetailSerializer, IssueDetailSerializer, \
     CommentDetailSerializer, ContributorDetailSerializer, \
     ProjectListSerializer, CommentListSerializer, IssueListSerializer, ContributorListSerializer
-from issuetrackingsystem.permissions import IsAdminAuthenticated
+from issuetrackingsystem.permissions import IsAdminAuthenticated, IsOwnerOrReadOnly, IsUserContributor
 
 # def get_contributor_permission(self, instance):
 #     contributor = Contributor.objects.get(project=instance, user=self.request.user)
@@ -15,68 +17,77 @@ from issuetrackingsystem.permissions import IsAdminAuthenticated
 #         return
 
 
-def check_owner(instance, user):
-    if type(instance) == Project:
-        contributor = Contributor.objects.get(project=instance, user=user)
-        if contributor.role == contributor.Role.CREATOR:
-            return True
-        return False
-    if type(instance) == Contributor:
-        if instance.role == Contributor.Role.CREATOR:
-            return True
-        return False
-    else:
-        if instance.author_user_id == user:
-            return True
-        return False
+# def check_owner(instance, user):
+#     if type(instance) == Project:
+#         contributor = Contributor.objects.get(project=instance, user=user)
+#         if contributor.role == contributor.Role.CREATOR:
+#             return True
+#         return False
+#     if type(instance) == Contributor:
+#         if instance.role == Contributor.Role.CREATOR:
+#             return True
+#         return False
+#     else:
+#         if instance.author_user_id == user:
+#             return True
+#         return False
 
 
-def check_user_in_contrib(project, user):
-    contributor = Contributor.objects.filter(project=project, user=user)
-    if contributor:
-        return True
-    else:
-        return False
+# def check_user_in_contrib(project, user):
+#     contributor = Contributor.objects.filter(project=project, user=user)
+#     if contributor:
+#         return True
+#     else:
+#         return False
 
 
-def get_project(self):
-    obj = self.get_object()
-    if type(obj) == Issue:
-        obj = obj.project
-    elif type(obj) == Comment:
-        obj = obj.issue_id.project
-    elif type(obj) == Contributor:
-        obj = obj.project
-    return obj
+# def get_project(self):
+#     obj = self.get_object()
+#     if type(obj) == Issue:
+#         obj = obj.project
+#     elif type(obj) == Comment:
+#         obj = obj.issue_id.project
+#     elif type(obj) == Contributor:
+#         obj = obj.project
+#     return obj
 
 
 class CustomViewset(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly, IsUserContributor]
 
-    def perform_create(self, serializer):
-        model = serializer.Meta.model
-        if model == Comment:
-            project = Issue.objects.get(id=self.request.data.get("issue_id")).project
-        else:
-            project = Project.objects.get(id=self.request.data.get("project"))
-        if check_user_in_contrib(project=project, user=self.request.user):
-            return serializer.save()
-        else:
-            raise ValueError("Vous ne pouvez pas créer d'objet si vous n'êtes pas contributeur du projet")
+    def create(self, request, *args, **kwargs):
+        project = Project.objects.get(id=self.request.data.get("project"))
+        if Contributor.objects.filter(project=project, user=self.request.user):
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        message = "Vous ne pouvez pas créer d'objet si vous n'êtes pas contributeur du projet"
+        return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
 
-    def perform_update(self, serializer):
-        if check_user_in_contrib(project=get_project(self), user=self.request.user) \
-                and check_owner(instance=self.get_object(), user=self.request.user):
-            return serializer.save()
-        else:
-            raise ValueError("Vous ne pouvez pas modifier un élement dont vous n'êtes pas le créateur")
-
-    def perform_destroy(self, instance):
-        if check_user_in_contrib(project=get_project(self), user=self.request.user) \
-                and check_owner(instance=self.get_object(), user=self.request.user):
-            return self.perform_destroy(instance)
-        else:
-            raise ValueError("Vous ne pouvez pas supprimer un élément dont vous n'êtes pas le créateur")
+    # def perform_create(self, serializer):
+    #     project = Project.objects.get(id=self.request.data.get("project"))
+    #     if Contributor.objects.filter(project=project, user=self.request.user):
+    #         return serializer.save()
+    #     # else:
+    #     #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     else:
+    #         raise ValueError("Vous ne pouvez pas créer d'objet si vous n'êtes pas contributeur du projet")
+#
+#     def perform_update(self, serializer):
+#         if check_user_in_contrib(project=get_project(self), user=self.request.user):
+#                 # and check_owner(instance=self.get_object(), user=self.request.user):
+#             return serializer.save()
+#         else:
+#             raise ValueError("Vous ne pouvez pas modifier un élement dont vous n'êtes pas le créateur")
+#
+#     def perform_destroy(self, instance):
+#         if check_user_in_contrib(project=get_project(self), user=self.request.user):
+#                 # and check_owner(instance=self.get_object(), user=self.request.user):
+#             return self.perform_destroy(instance)
+#         else:
+#             raise ValueError("Vous ne pouvez pas supprimer un élément dont vous n'êtes pas le créateur")
 
 
 class MultipleSerializerMixin:
@@ -203,3 +214,22 @@ class CommentViewset(MultipleSerializerMixin, CustomViewset):
         #     Q(author_user_id=self.request.user) |
         #     Q(issue_id__in=issues)
         # )
+
+    def create(self, request, *args, **kwargs):
+        project = Issue.objects.get(id=self.request.data.get("issue_id")).project
+        if Contributor.objects.filter(project=project, user=self.request.user):
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        message = "Vous ne pouvez pas créer d'objet si vous n'êtes pas contributeur du projet"
+        return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+    # def perform_create(self, serializer):
+    #     project = Issue.objects.get(id=self.request.data.get("issue_id")).project
+    #     if Contributor.objects.filter(project=project, user=self.request.user):
+    #         return serializer.save()
+    #     # else:
+    #     #     raise ValueError("Vous ne pouvez pas créer d'objet si vous n'êtes pas contributeur du projet")
+    #     return serializer.errors
