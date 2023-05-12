830f3aa7f19d31a26b229470f4f3ef8fd38109c8
Virginie PICH <red_iv@hotmail.fr>
2023-05-12 19:41:58 +0200

    Création du serializer et de la vue pour s'inscrire

--- softdesk/authentication/serializers.py
+++ softdesk/authentication/serializers.py
@@ -1,6 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
+# from rest_framework_simplejwt.serializer
+from rest_framework import serializers
 
 from authentication.models import User
+
+
+class MyTokenObtainPairSerializer():
+    pass
 
 
 class UserSerializer(ModelSerializer):
@@ -8,3 +14,41 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email", "password"]
+
+
+class CreateUserSerializer(ModelSerializer):
+
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'}, label="Confirmez votre mot de passe")
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "password2",
+        ]
+        extra_kwargs = {
+            "password": {"style": {'input_type': 'password'},
+                         "write_only": True}
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise ValidationError({"password": "Les champs de mot de passe ne sont pas identiques."})
+        elif not attrs["first_name"]:
+            raise ValidationError({"first_name": "Vous devez saisir un prénom."})
+        elif not attrs["last_name"]:
+            raise ValidationError({"last_name": "Vous devez saisir un nom de famille."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            password=validated_data["password"],
+        )
+
+        return user
--- softdesk/authentication/views.py
+++ softdesk/authentication/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from rest_framework.generics import CreateAPIView
 
-from authentication.serializers import UserSerializer
+from authentication.serializers import UserSerializer, CreateUserSerializer
 from authentication.models import User
 
 
@@ -8,5 +10,13 @@ class UserAdminViewset(ModelViewSet):
 
     serializer_class = UserSerializer
 
+    permission_classes = [IsAdminUser]
+
     def get_queryset(self):
         return User.objects.all()
+
+
+class SignupView(CreateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = CreateUserSerializer
