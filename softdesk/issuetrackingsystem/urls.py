abbb89e6da9360a2db1641e00ba0410e19551d05
Virginie PICH <red_iv@hotmail.fr>
2023-05-17 21:10:30 +0200

    Séparation des chemins urls dans leur application respective

---
+++ softdesk/issuetrackingsystem/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path, include
+from rest_framework_nested import routers
+
+from issuetrackingsystem.views import ProjectViewset, ContributorViewset, IssueViewset, CommentViewset, \
+    ProjectAdminViewset, IssueAdminViewset, CommentAdminViewset, ContributorAdminViewset
+
+router = routers.SimpleRouter()
+router.register("projects", ProjectViewset, basename="projects")
+
+projects_router = routers.NestedSimpleRouter(
+    router,
+    r"projects",
+    lookup="project"
+)
+projects_router.register(r"issues", IssueViewset, basename="issues")
+projects_router.register(r"users", ContributorViewset, basename="contributors")
+issues_router = routers.NestedSimpleRouter(
+    projects_router,
+    r"issues",
+    lookup="issue"
+)
+issues_router.register(r"comments", CommentViewset, basename="comments")
+
+router_admin = routers.SimpleRouter()
+router_admin.register("projects", ProjectAdminViewset, basename="admin-projects")
+router_admin.register("issues", IssueAdminViewset, basename="admin-issues")
+router_admin.register("comments", CommentAdminViewset, basename="admin-comments")
+router_admin.register("contributors", ContributorAdminViewset, basename="admin-contributors")
+
+urlpatterns = [
+    path("admin/", include(router_admin.urls)),
+    path("", include(router.urls)),
+    path("", include(projects_router.urls)),
+    path("", include(issues_router.urls)),
+]
--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -16,44 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_nested import routers
-
-from issuetrackingsystem.views import ProjectAdminViewset, IssueAdminViewset, CommentAdminViewset, ContributorAdminViewset, \
-    ProjectViewset, ContributorViewset, IssueViewset, CommentViewset
-from authentication.views import UserAdminViewset, SignupView
-
-router = routers.SimpleRouter()
-router.register("projects", ProjectViewset, basename="projects")
-
-projects_router = routers.NestedSimpleRouter(
-    router,
-    r"projects",
-    lookup="project"
-)
-projects_router.register(r"issues", IssueViewset, basename="issues")
-projects_router.register(r"users", ContributorViewset, basename="contributors")
-issues_router = routers.NestedSimpleRouter(
-    projects_router,
-    r"issues",
-    lookup="issue"
-)
-issues_router.register(r"comments", CommentViewset, basename="comments")
-
-
-router_admin = routers.SimpleRouter()
-router_admin.register("user", UserAdminViewset, basename="user")
-router_admin.register("projects", ProjectAdminViewset, basename="projects")
-router_admin.register("issues", IssueAdminViewset, basename="issues")
-router_admin.register("comments", CommentAdminViewset, basename="comments")
-router_admin.register("contributor", ContributorAdminViewset, basename="contributor")
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("rest_framework.urls")),
-    path("api/admin/", include(router_admin.urls)),
-    path("api/", include(router.urls)),
-    path("api/", include(projects_router.urls)),
-    path("api/", include(issues_router.urls)),
-    path("signup/", SignupView.as_view()),
+    path("api/accounts/", include("authentication.urls")),
+    path("api/", include("issuetrackingsystem.urls"))
 ]
